7dc736fa2e8e8c0569ddd65a5f1b3cce9f25e7a5
Liviu Cotiuga <lxc698@student.bham.ac.uk>
2019-01-08 15:14:37 +0000

    Implemented the Category Page for Footballs, cleaned the code, changed the images and adjusted the sizes of the divs

---
+++ js/filters.js
@@ -0,0 +1,221 @@
+function nikeCheckbox() {
+  var checkBox = document.getElementById("nike-checkbox");
+  var text = document.getElementById("nike-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function nikeMobileCheckbox() {
+//   var checkBox = document.getElementById("nike-mobilecheckbox");
+//   var text = document.getElementById("nike-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+function adidasCheckbox() {
+  var checkBox = document.getElementById("adidas-checkbox");
+  var text = document.getElementById("adidas-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function adidasMobileCheckbox() {
+//   var checkBox = document.getElementById("adidas-mobilecheckbox");
+//   var text = document.getElementById("adidas-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+function mitreCheckbox() {
+  var checkBox = document.getElementById("mitre-checkbox");
+  var text = document.getElementById("mitre-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function mitreMobileCheckbox() {
+//   var checkBox = document.getElementById("mitre-mobilecheckbox");
+//   var text = document.getElementById("mitre-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+function trainingCheckbox() {
+  var checkBox = document.getElementById("training-checkbox");
+  var text = document.getElementById("training-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function trainingMobileCheckbox() {
+//   var checkBox = document.getElementById("training-mobilecheckbox");
+//   var text = document.getElementById("training-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+function matchCheckbox() {
+  var checkBox = document.getElementById("match-checkbox");
+  var text = document.getElementById("match-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function matchMobileCheckbox() {
+//   var checkBox = document.getElementById("match-mobilecheckbox");
+//   var text = document.getElementById("match-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+function specialistCheckbox() {
+  var checkBox = document.getElementById("specialist-checkbox");
+  var text = document.getElementById("specialist-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function specialistMobileCheckbox() {
+//   var checkBox = document.getElementById("specialist-mobilecheckbox");
+//   var text = document.getElementById("specialist-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+function winterCheckbox() {
+  var checkBox = document.getElementById("winter-checkbox");
+  var text = document.getElementById("winter-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function winterMobileCheckbox() {
+//   var checkBox = document.getElementById("winter-mobilecheckbox");
+//   var text = document.getElementById("winter-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+function clearanceCheckbox() {
+  var checkBox = document.getElementById("clearance-checkbox");
+  var text = document.getElementById("clearance-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function clearanceMobileCheckbox() {
+//   var checkBox = document.getElementById("clearance-mobilecheckbox");
+//   var text = document.getElementById("clearance-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+function imsCheckbox() {
+  var checkBox = document.getElementById("ims-checkbox");
+  var text = document.getElementById("ims-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function imsMobileCheckbox() {
+//   var checkBox = document.getElementById("ims-mobilecheckbox");
+//   var text = document.getElementById("ims-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+function qualityCheckbox() {
+  var checkBox = document.getElementById("quality-checkbox");
+  var text = document.getElementById("quality-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function qualityMobileCheckbox() {
+//   var checkBox = document.getElementById("quality-mobilecheckbox");
+//   var text = document.getElementById("quality-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+function qualityproCheckbox() {
+  var checkBox = document.getElementById("qualitypro-checkbox");
+  var text = document.getElementById("qualitypro-cancel");
+  if (checkBox.checked == true){
+    text.style.display = "inline-block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function qualityproMobileCheckbox() {
+//   var checkBox = document.getElementById("qualitypro-mobilecheckbox");
+//   var text = document.getElementById("qualitypro-mobilecancel");
+//   if (checkBox.checked == true){
+//     text.style.display = "inline-block";
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+
